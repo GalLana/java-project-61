6134b22d24af41ca41447437b63193999642d19d
Svetlana Shmol <gallana.yu@gmail.com>
2024-08-26 23:30:04 +0300

    Fourth step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     application
+    id("checkstyle") // Подключение плагина checkstyle
 }
 
 group = "hexlet.code"
@@ -9,6 +10,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+checkstyle {
+    toolVersion = "10.12.4" // Укажите версию Checkstyle, соответствующую вашей среде
+    configFile = file("$rootDir/config/checkstyle/checkstyle.xml")
+}
+
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
@@ -25,4 +32,11 @@ tasks.test {
 
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+    }
 }
